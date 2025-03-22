eee8ab2096acc79c083bb1741f87c15eb71c1a8c
Minh Dao Nguyen <90809666+Minh-Dao-Nguyen@users.noreply.github.com>
2025-03-22 17:23:06 -0500

    progress

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/electron-vite.animate.svg'
 import './App.css'
+import {test} from "./services/mcpclient.ts";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,8 +19,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => {test}}>
+          TESTTTT
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
