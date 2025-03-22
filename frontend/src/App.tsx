0b5156f90a1537ca3d5a96bfaa262e71848ac3cd
justinbusker <jtbusker@icloud.com>
2025-03-22 12:00:58 -0500

    added router and tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -28,6 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+			<h1 className="text-5xl text-red-400">test</h1>
     </>
   )
 }
